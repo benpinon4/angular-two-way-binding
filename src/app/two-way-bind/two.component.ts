aa6adac2a6ac676617ae029a37e11db893de796b
benpinon4 <benjamin.pinon@CodeImmersives.com>
2025-04-09 10:41:35 -0400

    Explored angular signal and computed

--- src/app/two-way-bind/two.component.ts
+++ src/app/two-way-bind/two.component.ts
@@ -1,4 +1,4 @@
-import { Component } from "@angular/core";
+import { Component, signal, computed } from "@angular/core";
 
 
 @Component ({
@@ -7,14 +7,36 @@ import { Component } from "@angular/core";
     styleUrl: "two.component.css"
 })
 
-export class twoWay {   
+export class twoWay {
+    
+    dynamicVariable = signal("This is a Variable")
+    count = signal(0)
+    count2 = signal(0)
     name: string = "bob diamond"
     age: number = 55
     messageString!: string;
+    dynamicVariable2 = computed(()=> this.count2() + this.age)
     
     constructor(){
         this.messageString = `${this.name} is ${this.age} years old`
     }
 
+    changeVariable(){
+        console.log("clicked")
+        this.count.set(this.count()+1)
+        this.dynamicVariable.set(this.dynamicVariable() + this.count())
+        console.log(this.dynamicVariable())
+    }
+
+   consoleLogging() {
+    console.log(this.messageString)
+   }
+
+   addOne () {
+    this.count2.set(this.count2() + 1)
+    console.log(this.dynamicVariable2())
+   }
+    
+   
 
 }
